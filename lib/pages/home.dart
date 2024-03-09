8a2cffd6f236ac5183035d0ceb4ba66b56aafb22
Zyan <fislam183070@bscse.uiu.ac.bd>
2024-03-09 13:03:51 +0600

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
